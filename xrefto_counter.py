5e6636e556bae10e4bd021fc403152f294c01a2b
batcain <ntrcKm77J0@proton.me>
2023-07-13 10:04:58 +0300

    XrefTo counter

---
+++ xrefto_counter.py
@@ -0,0 +1,25 @@
+from idautils import *
+from idc import *
+from idaapi import *
+
+class dictionary(dict):
+    def __init__(self):
+        self = dict()
+
+    def add(self, key, value):
+        self[key] = value
+
+xref_disct = dictionary()
+
+for segea in Segments():
+    
+    if (idc.get_segm_name(segea) == ".text"): #check if in .text segment
+       for funcea in Functions(segea, get_segm_end(segea)):
+            count = 0
+            for xrefs in XrefsTo(funcea, flags=0):
+                if (str(XrefTypeName(xrefs.type)) in "Code_Near_Call" ):
+                    count+=1
+            if count != 0: # don't list functions without referation
+                xref_dict.add(hex(funcea), count)            
+
+print(sorted(xref_dict.items(), key=lambda x:x[1]))
